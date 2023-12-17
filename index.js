1df96cc6b7060bf9bdb4b679a7eab0cf51780d97
Rodrigo dos Santos <rdsa.rdsa@gmail.com>
2023-12-17 18:59:14 -0300

    correção

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-let heroi = "Espadachim Negro"
-let experiencia = "2500"
-let patente = "madeira"
+let heroi = "Espadachim Negro";
+let experiencia = 5000;
+let patente = "madeira";
 
-     
-        
-        if (experiencia >= 1000) { patente = "ferro"
-        }else if (experiencia = 1001 >= 2000) { patente = "bronze"
-                }else if (experiencia = 2001 >= 5000) { patente = "prata" 
-                        } else if (experiencia = 6001 >= 7000) { patente = "ouro"
-                                }else if (experiencia = 7001 >= 8000) { patente = "platina"
-                                        }else if (experiencia = 8001 >= 90000) { patente = "ascendente"
-                                                }else if (experiencia = 9001 >= 10000 ) { patente = "imortal"
-                                                        }else if (experiencia >= 10001) { patente = "radiante"
-        }
+if (experiencia >= 10000) {
+  patente = "radiante";
+} else if (experiencia >= 9001 && experiencia <= 10000) {
+  patente = "imortal";
+} else if (experiencia >= 8001 && experiencia <= 9000) {
+  patente = "ascendente";
+} else if (experiencia >= 7001 && experiencia <= 8000) {
+  patente = "platina";
+} else if (experiencia >= 6001 && experiencia <= 7000) {
+  patente = "ouro";
+} else if (experiencia >= 5001 && experiencia <= 6000) {
+  patente = "prata";
+} else if (experiencia >= 2001 && experiencia <= 5000) {
+  patente = "bronze";
+} else if (experiencia >= 1001 && experiencia <= 2000) {
+  patente = "ferro";
+}
 
-        console.log ("O Herói de nome " + heroi + " está nível "+ patente + "." )
-            
-        
+console.log("O Herói de nome " + heroi + " está nível " + patente + ".");
     
 
     
