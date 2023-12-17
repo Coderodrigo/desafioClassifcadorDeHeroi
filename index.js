f0307e84912cbd8dd09f3dae63c9817509d4d26a
Rodrigo dos Santos <rdsa.rdsa@gmail.com>
2023-12-17 17:54:43 -0300

    Atualização

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ let patente = "madeira"
         
         if (experiencia >= 1000) { patente = "ferro"
         }else if (experiencia = 1001 >= 2000) { patente = "bronze"
-        }else if (experiencia = 2001 >= 5000) { patente = "prata" 
-        } else if (experiencia = 6001 >= 7000) { patente = "ouro"
-        }else if (experiencia = 7001 >= 8000) { patente = "platina"
-        }else if (experiencia = 8001 >= 90000) { patente = "ascendente"
-        }else if (experiencia = 9001 >= 10000 ) { patente = "imortal"
-        }else if (experiencia >= 10001) { patente = "radiante"
-}
-            console.log ("O Herói de nome " + heroi + " está nível "+ patente + "." )
+                }else if (experiencia = 2001 >= 5000) { patente = "prata" 
+                        } else if (experiencia = 6001 >= 7000) { patente = "ouro"
+                                }else if (experiencia = 7001 >= 8000) { patente = "platina"
+                                        }else if (experiencia = 8001 >= 90000) { patente = "ascendente"
+                                                }else if (experiencia = 9001 >= 10000 ) { patente = "imortal"
+                                                        }else if (experiencia >= 10001) { patente = "radiante"
+        }
+
+        console.log ("O Herói de nome " + heroi + " está nível "+ patente + "." )
+            
         
     
 
